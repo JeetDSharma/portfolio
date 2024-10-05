277ed6637a83d35fcf0ddfc92930f4f3b5076d81
JeetDSharma <jeetsharma2112@gmail.com>
2024-10-05 19:59:08 -0400

    "updates"

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import Hero from "@/components/Hero";
 export default function Home() {
   return (
-    <main className="relaive bg-black-100">
-      <div><h1>Jeet's Portfolio</h1></div>
+    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 bg-black-100 ">
+      <div className="w-full max-w-7xl"><Hero/></div>
     </main>
   );
 }
