48173702888b2f40e5925f8347e83bb892b4eeaf
JeetDSharma <jeetsharma2112@gmail.com>
2024-10-06 00:40:18 -0400

    "Updated hero and nav section with component implementation"

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,9 +2,10 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { AuroraBackground } from './ui/aurora-backgrounds'
+import { Highlight, HeroHighlight } from './ui/hero-highlight';
 const Hero = () => {
   return (
-    <AuroraBackground>
+    <AuroraBackground className='max-h-[80vh]'>
     <motion.div
       initial={{ opacity: 0.0, y: 40 }}
       whileInView={{ opacity: 1, y: 0 }}
@@ -15,14 +16,16 @@ const Hero = () => {
       }}
       className="relative flex flex-col gap-4 items-center justify-center px-4"
     >
-      <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-        Background lights are cool you know.
-      </div>
+      <div className="text-3xl md:text-7xl font-bold uppercase dark:text-white text-center">
+  <Highlight>
+   Crafting the Future </Highlight>, <br /> One Revolutionary Idea at a Time
+</div>
+
       <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-        And this, is chemical burn.
+        This is Jeet Sharma
       </div>
       <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-        Debug now
+        Let's Dive In
       </button>
     </motion.div>
   </AuroraBackground>
