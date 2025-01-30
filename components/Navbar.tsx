8767224726ebcf68a27c4e061dc82ca8c7311b50
JeetDSharma <jeetsharma2112@gmail.com>
2025-01-30 12:02:29 -0500

    added hover on navbar buttons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,8 +32,8 @@ const Navbar = () => {
     <nav className="flex flex-row justify-between items-center px-6 py-2">
       <div className="text-2xl"> Jeet's Portfolio</div>
       <div className="flex space-x-8">
-        <button onClick={scrollToBody}> About Me</button>
-        <button onClick={downloadResume}> Resume</button>
+        <button className="hover:scale-105 transition-all duration-300" onClick={scrollToBody}> About Me</button>
+        <button className="hover:scale-105 transition-all duration-300" onClick={downloadResume}> Resume</button>
         <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
   <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl" onClick={scrollToContact}
